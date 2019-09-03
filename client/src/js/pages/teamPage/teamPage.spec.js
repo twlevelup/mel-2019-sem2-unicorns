59384c77430e71d7369815bf64ebeb33a7c1b8cd
Marielle <mcruz@ourveryown.com.au>
2019-09-03 20:09:35 +1000

    [Marielle] Add name Marielle to the team page

--- client/src/js/pages/teamPage/teamPage.spec.js
+++ client/src/js/pages/teamPage/teamPage.spec.js
@@ -1,28 +1,23 @@
-const TeamPage = require('./teamPage');
+const TeamPage = require("./teamPage");
 
-describe('TeamPage', () => {
+describe("TeamPage", () => {
   let watchFace;
   beforeEach(() => {
     document.body.innerHTML = `<div id='watch-face' style='height: 100px; width: 100px;'></div>`;
-    watchFace = document.getElementById('watch-face');
+    watchFace = document.getElementById("watch-face");
   });
 
-  describe('#render', () => {
-    it('should render my team page', () => {
+  describe("#render", () => {
+    it("should render my team page", () => {
       const page = new TeamPage();
       expect(page.render()).toContain("<h1>Made by:</h1>");
     });
 
-    it('should contain team members names', () => {
+    it("should contain team members names", () => {
       const page = new TeamPage();
       expect(page.render()).toContain("Patch");
+      expect(page.render()).toContain("Marielle");
+      expect(page.render()).toContain("Sue");
     });
-
   });
-
-  it('should contain team members names', () => {
-    const page = new TeamPage();
-    expect(page.render()).toContain("Sue");
-  });
-
 });
